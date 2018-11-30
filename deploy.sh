0abc1ea4787358c69636ceddcb00d09a9debf4fb
Tom Plüss <tom.pluess@simplificator.com>
2018-11-30 16:26:17 +0100

    Draft deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,22 @@
+#!/usr/bin/env bash
+set -e
+
+IMAGE=135594858514.dkr.ecr.eu-central-1.amazonaws.com/simplificator/kickstart:master
+
+echo Docker login...
+
+echo Pull new image...
+
+docker pull $IMAGE
+
+echo Kill all running containers...
+
+# docker service stop...
+docker stop $(docker ps -aq)
+docker rm $(docker ps -aq)
+
 echo Deploying...
+
+docker service create -d -p 80:80 $IMAGE
 
 echo Deploy finished.
